46001e5e64c2a71d4c831a8b05d74b731c610ae8
renuka0309 <renukaullegaddi03@gmail.com>
2026-04-14 20:01:48 +0530

    Add files via upload

---
+++ Renu_logical.java
@@ -0,0 +1,12 @@
+public class Renu_logical {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        
+        System.out.println(a>b && a!=b);
+        System.out.println(a<b && a==10);
+        System.out.println(!(a<b));
+        System.out.println(!(20<=5));
+    }
+        
+}
